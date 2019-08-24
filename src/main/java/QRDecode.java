f5c4d3a1c05723f6fc072e6ffbee134f76e9c9a9
Ankit Pati <contact@ankitpati.in>
2019-08-25 03:11:28 +0530

    try both binarizers; one should work

--- src/main/java/QRDecode.java
+++ src/main/java/QRDecode.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Map;
 import java.util.EnumMap;
+import java.awt.image.BufferedImage;
 
 import javax.imageio.ImageIO;
 
@@ -12,6 +13,7 @@ import com.google.zxing.NotFoundException;
 import com.google.zxing.DecodeHintType;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.common.HybridBinarizer;
+import com.google.zxing.common.GlobalHistogramBinarizer;
 import com.google.zxing.qrcode.QRCodeReader;
 
 public class QRDecode {
@@ -19,21 +21,31 @@ public class QRDecode {
             throws IOException, NotFoundException, ChecksumException,
                    FormatException {
 
+        BufferedImageLuminanceSource bilSource
+            = new BufferedImageLuminanceSource(ImageIO.read(qrCodeImage));
+
         Map<DecodeHintType, Boolean> hints
             = new EnumMap<DecodeHintType, Boolean> (DecodeHintType.class);
         hints.put(DecodeHintType.TRY_HARDER, true);
 
-        return new QRCodeReader()
-            .decode(
-                new BinaryBitmap(
-                    new HybridBinarizer(
-                        new BufferedImageLuminanceSource(
-                            ImageIO.read(qrCodeImage)
-                        )
-                    )
-                ),
-                hints
-            ).getText();
+        String decoded = "";
+
+        try {
+            decoded = new QRCodeReader()
+                .decode(
+                    new BinaryBitmap(new HybridBinarizer(bilSource)),
+                    hints
+                ).getText();
+        }
+        catch (Exception e) {
+            decoded = new QRCodeReader()
+                .decode(
+                    new BinaryBitmap(new GlobalHistogramBinarizer(bilSource)),
+                    hints
+                ).getText();
+        }
+
+        return decoded;
     }
 
     public static void main(String args[]) {
